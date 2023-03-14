cad963cbe807e1c4b5ccdcadbcfced92a38b67f4
Klaus <BitLCEntwicklerDortmund@web.de>
2023-03-14 11:20:55 +0100

    Add project files.

---
+++ MonkeyIsland_Inselsicht_Strand_zur_Kneipe/Insel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyIsland_Inselsicht_Strand_zur_Kneipe {
+    internal class Insel {
+        private Kneipe kneipe = new Kneipe();
+        private Strand strand = new Strand();
+        // Spielfigur ParkPlatz
+        private Spielfigur sfpp;
+
+        public void SetKneipe(Kneipe kneipe) {
+            this.kneipe = kneipe;
+        }
+
+        public Kneipe GetKneipe() {
+            return this.kneipe; 
+        }
+
+        public void SetStrand(Strand strand) {
+            this.strand = strand;
+        }
+
+        public Strand GetStrand() {
+            return this.strand; 
+        }
+
+        public void SetSfpp(Spielfigur sfpp) {
+            this.sfpp = sfpp;
+        }
+
+        public Spielfigur GetSfpp() {
+            return this.sfpp;
+        }
+    }
+}
---
+++ MonkeyIsland_Inselsicht_Strand_zur_Kneipe/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace MonkeyIsland_Inselsicht_Strand_zur_Kneipe {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, Kneipe - Good bye Strand!");
+
+            // lege Inselobjekt an
+            Insel insel = new Insel();
+
+            // lege Spielfigur an
+            Spielfigur guybrush = new Spielfigur();
+
+            // Anfangssituation:
+            // ================
+            // Sag der Insel,
+            // hol die Adresse vom Strand und
+            // setze das Objekt "guybrush" auf den Spielfigurparkplatz 
+            insel.GetStrand().SetSfpp(guybrush);
+            // Damit steht unse Spielfigur auf dem Strand!
+
+
+            // wir wollen, daß die Spielfigur vom Stand in die Kneipe geht:
+            // also fragen wir die Insel nach der Adresse der Kneipe
+            // und setzen die Spielfigur guybrush auf dem Spielfigurparkplatz der Kneipe
+            insel.GetKneipe().SetSfpp(guybrush);
+
+            // Problem:
+            // die Spielfigur ist somit 2x da - einmal am Strand und 2. in der Kneipe
+            // also - löschen wir das Spielfigurobjekt aus dem Spielfigurparkplatz, indem wir den Parkplatz auf null setzen
+            insel.GetStrand().SetSfpp(null);
+
+            Console.WriteLine(  insel.GetKneipe().GetSfpp() );
+            Console.WriteLine(  insel.GetKneipe().GetSfpp().GetHashCode() + "=Hashcode der Spielfigur in der Kneipe" );
+            Console.WriteLine(guybrush.GetHashCode()  + " Hashcode von Guybrush ") ;
+
+            Console.ReadLine(  );
+        }
+    }
+}
